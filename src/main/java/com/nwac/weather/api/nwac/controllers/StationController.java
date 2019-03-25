e253f6a2831f79f2193240112f6a297469ba4d1b
Kelly Crowther <crowther.kelly1@gmail.com>
2019-03-24 22:16:13 -0700

    create List of rows represented as HashMaps to store the table data; loop through the table cells to create the row of data for the HashMap; rename variables and add types to Station Model

--- src/main/java/com/nwac/weather/api/nwac/controllers/StationController.java
+++ src/main/java/com/nwac/weather/api/nwac/controllers/StationController.java
@@ -4,10 +4,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 import com.nwac.weather.api.nwac.models.Station;
 
@@ -18,16 +23,67 @@ public class StationController {
 
   @GetMapping("/stations")
   public Station getAllStations() throws IOException {
-    String bodyText = "";
+    String bodyText = new String();
+    List<HashMap<String, String>> allTableData = new ArrayList<HashMap<String, String>>();
+
     try {
       Document doc = Jsoup.connect("https://data.nwac.us/ALL").get();
       Element body = doc.body();
       bodyText = body.text();
       System.out.println(doc.body());
-      return new Station(bodyText);
+      return new Station(bodyText, allTableData);
     } catch (IOException e) {
       e.printStackTrace();
     }
-    return new Station(bodyText);
+    return new Station(bodyText, allTableData);
+  }
+
+  @GetMapping("/stations/{id}")
+  public Station getStation(@PathVariable(value="id") String stationId) throws IOException {
+    String bodyText = "";
+    HashMap<String, String> tableRow;
+    List<HashMap<String, String>> allTableData = new ArrayList<HashMap<String, String>>();
+    try {
+      Document doc = Jsoup.connect("https://data.nwac.us/" + stationId).get();
+      Element body = doc.body();
+      bodyText = body.text();
+      Integer dateHeaderIndex = bodyText.indexOf("MM");
+      Integer finalHeaderIndex = bodyText.indexOf("-----");
+      Integer finalDataIndex = bodyText.lastIndexOf("Total");
+      String headers = bodyText.substring(dateHeaderIndex, finalHeaderIndex);
+      String[] headersSplit = headers.split(" ");
+      String data = bodyText.substring(finalHeaderIndex, finalDataIndex).replace("-", " ").trim();
+      String[] allDataCells = data.split(" ");
+      // String day = bodyText.substring(finalHeaderIndex, endIndex);
+      System.out.println("Headers: " + headers);
+      Integer stringLength = allDataCells.length;
+      System.out.println("String length: " + headersSplit.length);
+      System.out.println("Data length: " + stringLength);
+      System.out.println(Arrays.toString(allDataCells));
+      
+      Integer counter = 0;
+      tableRow = new HashMap<>();
+
+      for (String cell: allDataCells) {
+        counter++;
+        System.out.println("Current Iteration: " + cell);
+        System.out.println("Counter: " + counter);
+        String myIndex = counter.toString();
+        tableRow.put(myIndex, cell);
+        System.out.println(tableRow);
+
+        if (counter == 11) {
+          allTableData.add(tableRow);
+          counter = 0;
+          tableRow = new HashMap<>();
+        }
+      }
+
+      System.out.println(allTableData);
+      return new Station(data, allTableData);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return new Station(bodyText, allTableData);
   }
 }
--- src/main/java/com/nwac/weather/api/nwac/models/Station.java
+++ src/main/java/com/nwac/weather/api/nwac/models/Station.java
@@ -1,13 +1,22 @@
 package com.nwac.weather.api.nwac.models;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Station {
   private final String body;
+  private final List<HashMap<String, String>> weather;
 
-  public Station(String body) {
+  public Station(String body, List<HashMap<String, String>> weather) {
     this.body = body;
+    this.weather = weather;
   }
 
   public String getBody() {
     return body;
+  }
+
+  public List<HashMap<String, String>> getWeather() {
+    return weather;
   }
 }
